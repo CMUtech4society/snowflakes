49fbebe80bf713e58947e1fcc6edfefc2e875c97
David Ankin <daveankin@gmail.com>
2020-01-18 01:16:43 -0500

    added some heroku stuff

--- config.js
+++ config.js
@@ -1,18 +1,27 @@
 require('dotenv').config({ path: require('path').join(__dirname, '.env') });
 
+
+var knexConfiguration = {
+  client: 'sqlite3',
+  connection: 'database.db',
+  useNullAsDefault: true
+  // client: 'mysql',
+  // connection: {
+  //   user: process.env['user'],
+  //   password: process.env['password'],
+  //   database: process.env['database']
+  // }
+};
+
+if (process.env.HEROKU)
+  knexOptions = {
+    client: 'mysql',
+    connection: process.env.CLEARDB_DATABASE_URL
+  };
+
 module.exports = {
   baseUrl: '/snowflakes',
-  title: 'Snow Flakes Backend',
-  cookieSecret: 'keyboards cat',
-  knexConfiguration: {
-    client: 'sqlite3',
-    connection: 'database.db',
-    useNullAsDefault: true
-    // client: 'mysql',
-    // connection: {
-    //   user: process.env['user'],
-    //   password: process.env['password'],
-    //   database: process.env['database']
-    // }
-  }
+  title: process.env.TITLE || 'Snow Flakes Backend',
+  cookieSecret: process.env.HEROKU_SECRET || 'keyboards cat',
+  knexConfiguration
 };
